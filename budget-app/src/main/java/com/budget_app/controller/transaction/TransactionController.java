d200008534a2a7626591d576aff174620fe622fe
craab220@gmail.com <99097952+chaseraab@users.noreply.github.com>
2026-01-18 16:47:10 -0600

    ✨ Adding work for transactions api search

--- budget-app/src/main/java/com/budget_app/controller/transaction/TransactionController.java
+++ budget-app/src/main/java/com/budget_app/controller/transaction/TransactionController.java
@@ -1,12 +1,19 @@
 package com.budget_app.controller.transaction;
 
 import com.budget_app.controller.base.BaseController;
+import com.budget_app.domain.transaction.Transaction;
 import com.budget_app.dto.transaction.TransactionRequest;
 import com.budget_app.dto.transaction.TransactionResponse;
 import com.budget_app.service.transaction.TransactionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/transactions/")
@@ -18,5 +25,10 @@ public class TransactionController extends BaseController<Long, TransactionReque
     public TransactionController(TransactionService transactionService) {
         super(transactionService);
         this.transactionService = transactionService;
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity<List<TransactionResponse>> searchTransactions(@RequestBody Map<String, Object> filters) {
+        return transactionService.searchTransactions(filters);
     }
 }
---
+++ budget-app/src/main/java/com/budget_app/searchCriteria/SearchCriteria.java
@@ -0,0 +1,8 @@
+package com.budget_app.searchCriteria;
+
+public class SearchCriteria {
+
+    private String key;
+    private String operation;
+    private Object value;
+}
